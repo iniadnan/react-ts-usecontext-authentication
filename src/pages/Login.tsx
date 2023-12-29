4b9d5b358844469c297cf679f0c89bfdca926f35
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-12-29 15:03:32 +0700

    loggin success

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { useTheme } from "../contexts/ThemeContext"
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Login() {
 
     const { darkMode } = useTheme();
+    const { login } = useAuth()
+    const navigate = useNavigate();
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        login()
+        return navigate("/dashboard");
+    }
 
     return (
         <main className={`${darkMode ? 'bg-slate-900' : 'bg-slate-50'} w-full`}>
             <div className="container mx-auto px-5 md:px-0 pt-40 pb-40">
-                <form className="w-[750px] flex items-start mx-auto bg-slate-100 p-5 rounded">
+                <form className="w-[750px] flex items-start mx-auto bg-slate-100 p-5 rounded" onSubmit={onSubmit}>
                     <div className="w-5/12">
                         <img className="w-full h-[300px] object-cover" src="/images/login-image.jpg" alt="Login" height="100" width="100" />
                     </div>
