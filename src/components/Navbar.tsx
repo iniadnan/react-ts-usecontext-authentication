a515b338bf679ff7b90f408171d0b5d51502e882
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-12-29 16:25:36 +0700

    logout systems

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import { useTheme } from "../contexts/ThemeContext"
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Navbar() {
 
     const { darkMode } = useTheme();
+    const { isAuthenticated, logout } = useAuth();
 
     return (
         <nav className={`${darkMode ? 'bg-slate-900 border-gray-800' : 'bg-slate-50 border-gray-200'} w-full border-b-2`}>
@@ -16,7 +18,7 @@ export default function Navbar() {
                     <h3 className={`${darkMode ? 'text-purple-500' : 'text-purple-800'} font-medium text-xl`}>AuthenticationToggleTheme</h3>
                 </div>
                 <div className="w-4/12 flex justify-end items-center gap-x-5">
-                    <Link className={`${darkMode ? 'text-gray-100' : 'text-gray-500'} font-medium text-base`} to="login">Login</Link>
+                    {isAuthenticated ? <button onClick={logout} type="button"><img className="object-cover rounded-full" src="/images/user.jpg" height="40" width="40" /></button> : <Link className={`${darkMode ? 'text-gray-100' : 'text-gray-500'} font-medium text-base`} to='login'>Login</Link>}
                 </div>
             </div>
         </nav>
